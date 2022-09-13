61fd6378539dd27e70a8db46ab397f1495d920ce
jayasurya <jayasuryavs1999@gmail.com>
2022-09-13 17:41:18 +0530

    updated

--- index.js
+++ index.js
@@ -68,65 +68,72 @@ app.get("/", async function (req, res) {
 //   agent.handlerequest(intentmap);
 // });
 
-app.post("/chat", async function (req, res) {
+app.post("/chat", async function (request, response) {
   // var request = require("request");
-  try {
-    var city = "krypton";
-    city = req.body.queryResult.parameters.city;
-    req.body.queryResult.parameters["city"];
-    console.log("city" + " " + city);
-    if (city == null) {
-      city = "singapore";
-    }
-    let data = [];
-    let weatherapi =
-      "https://api.openweathermap.org/data/2.5/forecast?q=" +
-      city +
-      "&appid=c1bea648f3c6db74595c7c465820709c";
-    await axios(weatherapi)
-      .then((res) => {
-        data.push(res.data.list[0].weather[0].description);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-    // res.json(data);
 
-    // console.log(data);
-
-    res.send(JSON.stringify({ weather: data }));
-    console.log(`Current weather in ${city} will be ${data}`);
-  } catch (error) {
-    error;
+  var city = "krypton";
+  city = req.body.queryResult.parameters.city;
+  request.body.queryResult.parameters["city"];
+  console.log("city" + " " + city);
+  if (city == null) {
+    city = "singapore";
   }
-
-  // var obj = JSON.parse(body);
-  // console.log(obj);
-
-  // var list = obj.list[0];
-  // console.log("total length : " + obj.list.length);
-  // var tempDate = new Date("2022-09-10T12:00:00+09:00");
-  // console.log("tempdate month is " + tempDate.getMonth());
-
-  // var s = "";
-  // var description = "none found";
-  // for (var i = 0; i < obj.list.length; i++) {
-  //   var date = new Date(obj.list[i].dt * 1000);
-
-  //   var s = "";
-  //   var description = "none found";
-  //   for (var i = 0; i < obj.list.length; i++) {
-  //     var date = new Date(obj.list[i].dt * 1000);
-  //     var weather = obj.list[i].weather[0].description;
-
-  //     console.log("monthis " + date.getMonth());
-  //     console.log("the day is " + date.getDate());
-
-  //     if (date.getMonth() == month && date.getDate() == day) {
-  //       description = weather;
-  //       description = description + " " + date.toString();
-  //     }
-  //   }
-  // }
+  let data = [];
+  let weatherapi =
+    "https://api.openweathermap.org/data/2.5/forecast?q=" +
+    city +
+    "&appid=c1bea648f3c6db74595c7c465820709c";
+  await axios(weatherapi)
+    .then((response) => {
+      data.push(response.data.list[0].weather[0].description);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  response.send(
+    JSON.stringify({
+      data: "hi",
+      fullfillment: data,
+    })
+  );
+  console.log(`Current weather in ${city} will be ${data}`);
 });
+
+//   {
+//     response.setHeader('Content-Type', 'application/json'),
+//     response.send(JSON.stringify({
+//         "speech" : "Couldn't find any deatails. :(  ",
+//         "displayText" : "Couldn't find any deatails. :(  "
+//     }));
+// }
+
+// var obj = JSON.parse(body);
+// console.log(obj);
+
+// var list = obj.list[0];
+// console.log("total length : " + obj.list.length);
+// var tempDate = new Date("2022-09-10T12:00:00+09:00");
+// console.log("tempdate month is " + tempDate.getMonth());
+
+// var s = "";
+// var description = "none found";
+// for (var i = 0; i < obj.list.length; i++) {
+//   var date = new Date(obj.list[i].dt * 1000);
+
+//   var s = "";
+//   var description = "none found";
+//   for (var i = 0; i < obj.list.length; i++) {
+//     var date = new Date(obj.list[i].dt * 1000);
+//     var weather = obj.list[i].weather[0].description;
+
+//     console.log("monthis " + date.getMonth());
+//     console.log("the day is " + date.getDate());
+
+//     if (date.getMonth() == month && date.getDate() == day) {
+//       description = weather;
+//       description = description + " " + date.toString();
+//     }
+//   }
+// }
+
 app.listen(PORT, () => console.log(`server is  live on port ${PORT}`));
